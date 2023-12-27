a6f5c089725bb514990894ec0b562f701e132cc3
M. Rizky Kurniawan Lasim <111512177+JustRizky@users.noreply.github.com>
2023-12-27 11:09:21 +0700

    Add files via upload

---
+++ src/Model/Json/ModelJsonUser.java
@@ -0,0 +1,16 @@
+package Model.Json;
+
+public class ModelJsonUser {
+    public String pk;
+    public String namaUser;
+    public String nomorRekening;
+    public String pin;
+
+    public ModelJsonUser() {
+        this.pk = "pk";
+        this.namaUser = "namaUser";
+        this.nomorRekening = "nomorRekening";
+        this.pin = "pin";
+    }
+
+}
